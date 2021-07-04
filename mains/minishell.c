d1ecae0ae12c37802ff8ddd23483d0203efc2d3a
jsimonis <jsimonis@student.codam.nl>
2021-07-04 14:57:01 +0200

    why do you have not norm so much, like really, it takes 1 minute to normify EVERYTHING, WHYYYY, you dont even allow empty lines

--- mains/minishell.c
+++ mains/minishell.c
@@ -10,38 +10,43 @@
 #include "minishell.h"
 #include "executable.h"
 
-static void executable_from_command(const t_list* cmd, t_executable* o_executable)
+static void	executable_from_command(const t_list *cmd, t_executable *
+	o_executable)
 {
-	executable_init(o_executable, *(char**)list_index(cmd, 0));
-	for (size_t i = 1; i < cmd->count; i++)
-		executable_add_arg(o_executable,  *(char**)list_index(cmd, i));
+	size_t	i;
+
+	executable_init(o_executable, *(char **)list_index(cmd, 0));
+	i = 1;
+	while (i < cmd->count)
+	{
+		executable_add_arg(o_executable, *(char **)list_index(cmd, i));
+		i++;
+	}
 }
 
-int	main(int argc, char** argv, const char** envp)
+//list_un_init(&cmd, free);
+//list_un_init(&cmd, free);
+
+int	main(int argc, char **argv, const char **envp)
 {
+	t_executable	executable;
+	pid_t			pid;
+	t_list			cmd;
+	int				status;
+
 	env_copy_ptr(envp);
 	(void)argc;
 	(void)argv;
-
-
-	t_list	cmd;
-	t_executable executable;
-
 	while (true)
 	{
 		cmd = command_read();
 		if (cmd.count < 1)
 		{
-			//list_un_init(&cmd, free);
-			continue;
+			continue ;
 		}
 		executable_from_command(&cmd, &executable);
-
-		pid_t pid = executable_run(&executable);
-		int status;
+		pid = executable_run(&executable);
 		waitpid(pid, &status, 0);
-
 		executable_un_init(&executable);
-		//list_un_init(&cmd, free);
 	}
 }
