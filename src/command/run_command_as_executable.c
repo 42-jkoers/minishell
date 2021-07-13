dfac2c070e3bc7f0b214ef0b96c4a83665ebdd1a
SirMorfield <joppe.koers@gmail.com>
2021-07-13 17:55:51 +0200

    adding redirects

--- src/command/run_command_as_executable.c
+++ src/command/run_command_as_executable.c
@@ -3,80 +3,60 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "minishell.h"
 #include "executable.h"
 #include "command_read.h"
 #include "malloc_wrappers.h"
+#include "ft_list.h"
 
-// TODO: error when no right expression on grammar rule
-// TODO: output redirect
-t_executable
-	*push_executables(t_list *executables, const t_list *cmd, size_t i)
-{
-	t_executable	exec;
-	t_executable	*new;
-	t_block			*block;
-
-	executable_init(&exec, ((t_block *)list_index(cmd, i))->text);
-	i++;
-	while (i < cmd->count)
-	{
-		block = (t_block *)list_index_unchecked(cmd, i);
-		if (block->type & B_GRAMMAR_RULE)
-		{
-			new = push_executables(executables, cmd, i + 1);
-			if (block->type == B_PIPE)
-				executable_add_pipe(&exec, new);
-			break ;
-		}
-		else
-			executable_add_arg(&exec, block->text);
-		i++;
-	}
-	list_push_safe(executables, &exec);
-	return (list_index_unchecked(executables, executables->count - 1));
-}
-
-static void	run_all(pid_t *pids, int *statuses, t_list *executables)
+static void	run_all(pid_t *pids, int *statuses, t_list *execs)
 {
 	size_t	i;
 
-	i = executables->count;
-	while (i)
+	i = 0;
+	while (i < execs->count)
 	{
-		i--;
-		pids[i] = executable_run(list_index_unchecked(executables, i));
+		pids[i] = executable_run(list_index_unchecked(execs, i));
+		i++;
 	}
-	i = executables->count;
-	while (i)
+	i = 0;
+	while (i < execs->count)
 	{
-		i--;
 		waitpid(pids[i], &statuses[i], 0);
+		i++;
 	}
-	i = executables->count;
-	while (i)
+	i = 0;
+	while (i < execs->count)
 	{
-		i--;
-		executable_un_init(list_index_unchecked(executables, i));
+		executable_un_init(list_index_unchecked(execs, i));
+		i++;
 	}
 }
 
 // TODO: do something with return value of waitpid?
-// TODO: exit code??
+// TODO: exit code / return value?
+// TODO: better error when invalid grammar rule
 int	run_command_as_executable(const t_list *cmd)
 {
 	pid_t			*pids;
 	int				*statuses;
-	t_list			executables;
+	t_list			execs;
 
-	list_init_safe(&executables, sizeof(t_executable));
-	push_executables(&executables, cmd, 0);
-	pids = ft_malloc(executables.count * sizeof(pid_t));
-	statuses = ft_malloc(executables.count * sizeof(int));
-	run_all(pids, statuses, &executables);
-	list_un_init(&executables, NULL);
+	if (command_contains_invalid_grammar_rule(cmd))
+	{
+		printf("Invalid redirect or pipe\n");
+		return (1);
+	}
+	list_init_safe(&execs, sizeof(t_executable));
+	push_execs(&execs, cmd);
+	pids = ft_malloc(execs.count * sizeof(pid_t));
+	statuses = ft_malloc(execs.count * sizeof(int));
+	run_all(pids, statuses, &execs);
 	free(pids);
 	free(statuses);
+	list_un_init(&execs, NULL);
 	return (0);
 }
