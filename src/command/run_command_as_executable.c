f5450521acc507a11267c8611f0871e931ffade1
SirMorfield <joppe.koers@gmail.com>
2021-07-13 23:01:13 +0200

    run_command_as_executable() now returns correct exit code

--- src/command/run_command_as_executable.c
+++ src/command/run_command_as_executable.c
@@ -12,51 +12,43 @@
 #include "malloc_wrappers.h"
 #include "ft_list.h"
 
-static void	run_all(pid_t *pids, int *statuses, t_list *execs)
+static int	run_all(t_list *execs)
 {
 	size_t	i;
+	int		wstatus;
+	int		exit_status;
+	pid_t	pid;
 
 	i = 0;
+	exit_status = 0;
 	while (i < execs->count)
 	{
-		pids[i] = executable_run(list_index_unchecked(execs, i));
+		pid = executable_run(list_index_unchecked(execs, i));
+		if (waitpid(pid, &wstatus, 0) == -1)
+			exit_with_error("waitpid failed");
+		if (WIFEXITED(wstatus))
+		{
+			exit_status = WEXITSTATUS(wstatus);
+			if (exit_status)
+				break ;
+		}
 		i++;
 	}
-	i = 0;
-	while (i < execs->count)
-	{
-		waitpid(pids[i], &statuses[i], 0);
-		i++;
-	}
-	i = 0;
-	while (i < execs->count)
-	{
-		executable_un_init(list_index_unchecked(execs, i));
-		i++;
-	}
+	list_foreach(execs, (t_foreach_value)executable_un_init);
+	return (exit_status);
 }
 
-// TODO: do something with return value of waitpid?
-// TODO: exit code / return value?
 // TODO: better error when invalid grammar rule
 int	run_command_as_executable(const t_list *cmd)
 {
-	pid_t			*pids;
-	int				*statuses;
+	int				exit_status;
 	t_list			execs;
 
 	if (command_contains_invalid_grammar_rule(cmd))
-	{
-		printf("Invalid redirect or pipe\n");
-		return (1);
-	}
+		exit_with_error("Invalid redirect or pipe\n");
 	list_init_safe(&execs, sizeof(t_executable));
 	push_execs(&execs, cmd);
-	pids = ft_malloc(execs.count * sizeof(pid_t));
-	statuses = ft_malloc(execs.count * sizeof(int));
-	run_all(pids, statuses, &execs);
-	free(pids);
-	free(statuses);
+	exit_status = run_all(&execs);
 	list_un_init(&execs, NULL);
-	return (0);
+	return (exit_status);
 }
