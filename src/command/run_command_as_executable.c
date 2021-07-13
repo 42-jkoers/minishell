5c8ccc0687ae4c71277e9d2b66e244934c979512
SirMorfield <joppe.koers@gmail.com>
2021-07-13 23:07:07 +0200

    same functionality, better code

--- src/command/run_command_as_executable.c
+++ src/command/run_command_as_executable.c
@@ -16,26 +16,19 @@ static int	run_all(t_list *execs)
 {
 	size_t	i;
 	int		wstatus;
-	int		exit_status;
 	pid_t	pid;
 
 	i = 0;
-	exit_status = 0;
 	while (i < execs->count)
 	{
 		pid = executable_run(list_index_unchecked(execs, i));
 		if (waitpid(pid, &wstatus, 0) == -1)
 			exit_with_error("waitpid failed");
-		if (WIFEXITED(wstatus))
-		{
-			exit_status = WEXITSTATUS(wstatus);
-			if (exit_status)
-				break ;
-		}
+		if (WIFEXITED(wstatus) && WEXITSTATUS(wstatus))
+			return (WEXITSTATUS(wstatus));
 		i++;
 	}
-	list_foreach(execs, (t_foreach_value)executable_un_init);
-	return (exit_status);
+	return (0);
 }
 
 // TODO: better error when invalid grammar rule
@@ -49,6 +42,6 @@ int	run_command_as_executable(const t_list *cmd)
 	list_init_safe(&execs, sizeof(t_executable));
 	push_execs(&execs, cmd);
 	exit_status = run_all(&execs);
-	list_un_init(&execs, NULL);
+	list_un_init(&execs, (t_foreach_value)executable_un_init);
 	return (exit_status);
 }
