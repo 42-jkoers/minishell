8022e1ccebb19b2737f702b0f62b709f14b81721
SirMorfield <joppe.koers@gmail.com>
2021-07-24 16:11:23 +0200

    shut up norm

--- include/functions/signal_handler.h
+++ include/functions/signal_handler.h
@@ -7,12 +7,11 @@ typedef enum e_controll_c_pressed_status
 {
 	f_pressed = 1,
 	f_pressed_while_executing = 2,
-
 	pressed = f_pressed,
 	pressed_while_executing = f_pressed | f_pressed_while_executing
 }	t_controll_c_pressed_status;
 
-void	setup_signals(void);
+void						setup_signals(void);
 t_controll_c_pressed_status	*controll_c_pressed(void);
 
 #endif
