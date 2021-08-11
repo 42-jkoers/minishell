e90afc046d4cabd5c34ac8ddf16bc35d87d03fd6
SirMorfield <joppe.koers@gmail.com>
2021-08-11 13:56:37 +0200

    step 3a returning blocks instead of char *

--- src/command/command_read/3_cmd_to_blocks.c
+++ src/command/command_read/3_cmd_to_blocks.c
@@ -4,17 +4,22 @@
 #include <stdlib.h>
 
 // TODO check for invalid cmd_split
+// TODO give correct type
 bool	cmd_to_blocks(t_list *blocks, const t_list *cmd_split)
 {
 	size_t	i;
 	char	*current;
+	t_block	block;
 
+	list_init(blocks, sizeof(t_block));
 	i = 0;
 	while (i < cmd_split->count)
 	{
-		current = list_index_unchecked(cmd_split, i);
-		expand_environment_variables(&current);
-		remove_quotes(&current);
+		block.text = mc(ft_strdup(list_index_unchecked(cmd_split, i)));
+		expand_environment_variables(&block.text);
+		remove_quotes(&block.text);
+		block.type = B_NORMAL;
+		list_push_safe(blocks, &block);
 		i++;
 	}
 	return (true);
