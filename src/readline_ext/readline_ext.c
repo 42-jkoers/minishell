824814e49c8c4c433b5114e06328e899b1a14773
SirMorfield <joppe.koers@gmail.com>
2021-08-11 17:45:33 +0200

    add history to readline_ext

--- src/readline_ext/readline_ext.c
+++ src/readline_ext/readline_ext.c
@@ -1,11 +1,19 @@
 #include "readline_ext.h"
 #include "signal_handler.h"
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "libft.h"
 #include <readline/readline.h>
-#include <readline/readline.h>
+#include <readline/history.h>
 
 //printf("got line: %s and c_pressed: %i\n", *o_read, *controll_c_pressed());
+
+bool	is_empty_cmd(const char *cmd)
+{
+	while (ft_isspace(*cmd))
+		cmd++;
+	return (!(*cmd));
+}
 
 t_readline_ret_type	readline_ext(const char *promt, char **o_read)
 {
@@ -17,5 +25,11 @@ t_readline_ret_type	readline_ext(const char *promt, char **o_read)
 		return (CONTROL_D);
 	if (*controll_c_pressed() & f_pressed)
 		return (CONTROL_C);
+	if (is_empty_cmd(*o_read))
+	{
+		free(*o_read);
+		return (readline_ext(promt, o_read));
+	}
+	add_history(*o_read);
 	return (NORMAL);
 }
