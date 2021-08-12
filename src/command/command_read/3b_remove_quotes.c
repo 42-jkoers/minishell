56cd7328882ded180b3e94b2d950ef076d8989e1
SirMorfield <joppe.koers@gmail.com>
2021-08-12 20:59:41 +0200

    bugfix remove_quotes()

--- src/command/command_read/3b_remove_quotes.c
+++ src/command/command_read/3b_remove_quotes.c
@@ -8,33 +8,27 @@
 #include <stdio.h>
 #include "ft_ternary.h"
 
-void	loop_chars(t_list *text, const char *start)
-{
-	char	quote;
-	char	*current;
-
-	current = (char *)start;
-	quote = ter_char(type_quote(*start), *start, 0);
-	if (type_quote(*current))
-		current++;
-	while (*current)
-	{
-		if (!(quote && type_quote(*current)))
-			list_push_safe(text, current);
-		if (type_quote(*current))
-			quote = ter_char(quote == *current, 0, *current);
-		current++;
-	}
-}
-
-// expects mallocd string
 void	remove_quotes(char **str)
 {
-	t_list	text;
+	char	*src;
+	char	*dst;
+	char	quote;
 
-	list_init_safe(&text, sizeof(char));
-	loop_chars(&text, *str);
-	list_push_safe(&text, "");
-	free(*str);
-	*str = text.data;
+	src = *str;
+	dst = *str;
+	quote = 0;
+	while (*src && *dst)
+	{
+		if ((type_quote(*src) && !quote) || quote == *src)
+		{
+			quote = ter_char(quote == *src, 0, *src);
+			src++;
+			continue ;
+		}
+		if (dst != src)
+			*dst = *src;
+		dst++;
+		src++;
+	}
+	*dst = 0;
 }
